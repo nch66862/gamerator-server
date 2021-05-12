40026009aa4ca7c7db21d2e721cfb2462ad390c6
Nick Carver <nicholascarver2@outlook.com>
2021-05-12 11:38:34 -0500

    following some tutorial online. nothing works yet so what is the harm

--- gamerator/settings.py
+++ gamerator/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -42,6 +43,14 @@ INSTALLED_APPS = [
     'corsheaders',
     'gameratorapi',
 ]
+
+AUTH_USER_MODEL = "gameratorapi.models.Player"
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "gameratorapi", "site_static")]
+STATIC_ROOT = os.path.join(BASE_DIR, "gameratorapi", "static")
+STATIC_URL = "/static/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "gameratorapi", "media")
+MEDIA_URL = "/media/"
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
--- gameratorapi/models/picture.py
+++ gameratorapi/models/picture.py
@@ -1,7 +1,15 @@
 from django.db import models
+from django.utils import timezone
+import os
+
+def upload_to(instance, filename):
+    now = timezone.now()
+    base, extension = os.path.splitext(filename.lower())
+    milliseconds = now.microsecond // 1000
+    return f"users/{instance.pk}/{now:%Y%m%d%H%M%S}{milliseconds}{extension}"
 
 class Picture(models.Model):
 
     game = models.ForeignKey("Game", on_delete=models.CASCADE)
     player = models.ForeignKey("Player", on_delete=models.CASCADE)
-    image = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=100)
+    image = models.ImageField(upload_to=upload_to, height_field=None, width_field=None)
